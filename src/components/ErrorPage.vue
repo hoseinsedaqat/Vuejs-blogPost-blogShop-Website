b3fdf7fffa839a48f06c584fee742d763c31cb34
hsedaqat1378 <71232870+hsedaqat1378@users.noreply.github.com>
2021-01-19 16:04:43 +0330

    Create ErrorPage.vue

---
+++ src/components/ErrorPage.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <app-header></app-header>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="card p-4 mt-5 border-danger">
+            <h3>Error 404 Page is Not Found</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from "./Header.vue";
+export default {
+  name: "ErrorPage",
+  components: {
+    "app-header": Header,
+  },
+};
+</script>
+
+<style></style>
