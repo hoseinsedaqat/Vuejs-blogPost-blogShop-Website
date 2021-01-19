fbf3d3573e085203ae3841771978a18576a7b0de
hsedaqat1378 <71232870+hsedaqat1378@users.noreply.github.com>
2021-01-19 15:53:09 +0330

    Update router.js

--- src/Router/router.js
+++ src/Router/router.js
@@ -4,6 +4,10 @@ import Login from '../components/Login.vue'
 import ReadMore from '../components/ReadMore.vue'
 import Shop from '../components/Shop.vue'
 import Cart from '../components/Cart.vue'
+import ReadMoreProduct from '../components/ReadMoreProduct.vue'
+import Setting from '../components/Setting.vue'
+import Message from '../components/Message.vue'
+import ErrorPage from '../components/ErrorPage.vue'
 
 
 export const router = [
@@ -12,5 +16,10 @@ export const router = [
     { path: '/Blog', component: Blog },
     { path: '/Blog/:id', component: ReadMore },
     { path: '/Shop', component: Shop },
-    { path: '/Cart', component: Cart }
+    { path: '/Cart', component: Cart },
+    { path: '/Product/:id', component: ReadMoreProduct },
+    { path: '/Setting', component: Setting },
+    { path: '/Message', component: Message },
+    // Error 404
+    { path: '*', component: ErrorPage }
 ]
