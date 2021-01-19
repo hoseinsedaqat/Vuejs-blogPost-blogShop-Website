759b2b7d4c6ac1969f4a3643f5d942b76e5f6f5a
hsedaqat1378 <71232870+hsedaqat1378@users.noreply.github.com>
2021-01-19 16:06:05 +0330

    Create readmoreProduct.js

---
+++ src/data/readmoreProduct.js
@@ -0,0 +1,47 @@
+export default [
+    {
+        number: 1,
+        name: 'Pencil',
+        aboutPorduct: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum deleniti veniam ab nobis laudantium impedit voluptate
+        harum fugiat. Laudantium saepe cum dolorem dignissimos, vel quidem deserunt mollitia dolorum qui harum, excepturi,
+        aperiam veritatis enim numquam quas eaque! Quae, architecto. Minus impedit unde facilis, beatae neque molestiae quae
+        porro enim saepe ullam? Asperiores vitae nobis voluptas quaerat placeat quod saepe quo quibusdam accusantium, veritatis
+        eveniet nam! Dolor at vero recusandae! Rem fugiat exercitationem nulla, officiis a autem quas illo!`,
+        product: {
+            number: 1,
+            name: 'Pencil',
+            price: '5$',
+            quantity: "1"
+        }
+    },
+    {
+        number: 2,
+        name: 'NoteBook',
+        aboutPorduct: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum deleniti veniam ab nobis laudantium impedit voluptate
+        harum fugiat. Laudantium saepe cum dolorem dignissimos, vel quidem deserunt mollitia dolorum qui harum, excepturi,
+        aperiam veritatis enim numquam quas eaque! Quae, architecto. Minus impedit unde facilis, beatae neque molestiae quae
+        porro enim saepe ullam? Asperiores vitae nobis voluptas quaerat placeat quod saepe quo quibusdam accusantium, veritatis
+        eveniet nam! Dolor at vero recusandae! Rem fugiat exercitationem nulla, officiis a autem quas illo!s`,
+        product: {
+            number: 2,
+            name: 'NoteBook',
+            price: '10$',
+            quantity: "1"
+        }
+    },
+    {
+        number: 3,
+        name: 'Backpack',
+        aboutPorduct: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum deleniti veniam ab nobis laudantium impedit voluptate
+        harum fugiat. Laudantium saepe cum dolorem dignissimos, vel quidem deserunt mollitia dolorum qui harum, excepturi,
+        aperiam veritatis enim numquam quas eaque! Quae, architecto. Minus impedit unde facilis, beatae neque molestiae quae
+        porro enim saepe ullam? Asperiores vitae nobis voluptas quaerat placeat quod saepe quo quibusdam accusantium, veritatis
+        eveniet nam! Dolor at vero recusandae! Rem fugiat exercitationem nulla, officiis a autem quas illo!`,
+        product: {
+            number: 3,
+            name: 'Backpack',
+            price: '20$',
+            quantity: "1"
+        }
+    },
+]
