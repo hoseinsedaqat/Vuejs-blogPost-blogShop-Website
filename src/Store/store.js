92224896faeb2131af2666679b95d8ec843abc91
hsedaqat1378 <71232870+hsedaqat1378@users.noreply.github.com>
2021-02-05 13:00:45 +0330

    Update store.js

--- src/Store/store.js
+++ src/Store/store.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import data from '../data/data.js'
 import product from '../data/product.js'
+import ProductData from '../data/ProductData.js'
 import getters from './getters.js'
 import actions from './actions.js'
 import mutations from './mutations.js'
@@ -11,7 +12,8 @@ Vue.use(Vuex)
 export const store = new Vuex.Store({
     state: {
         data,
-        product
+        product,
+        ProductData
     },
     getters,
     mutations,
