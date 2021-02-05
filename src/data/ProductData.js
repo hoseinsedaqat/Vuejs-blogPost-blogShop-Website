a5319b3c154e6dc3bed3da35c9152dc30aa4c711
hsedaqat1378 <71232870+hsedaqat1378@users.noreply.github.com>
2021-02-05 12:58:29 +0330

    Create ProductData.js

---
+++ src/data/ProductData.js
@@ -0,0 +1,25 @@
+// import Image from Assets And use in Data
+import imgOne from '../assets/1.jpg'
+import imgTwo from '../assets/2.jpg'
+import imgThree from '../assets/3.jpg'
+export default [
+    {
+        name: 'Blog Post Pencil',
+        price: '5$',
+        img: imgOne,
+        quantity: '1'
+    },
+    {
+        name: 'Blog Post Book',
+        price: '10$',
+        img: imgTwo,
+        quantity: '1'
+    },
+    {
+        name: 'Blog Post BackPack',
+        price: '20$',
+        img: imgThree,
+        quantity: '1'
+    }
+
+]
