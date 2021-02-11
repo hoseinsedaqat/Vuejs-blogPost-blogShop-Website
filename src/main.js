8dbdf757fcd5c2463e789eda2fcb04b6995a70e5
hsedaqat1378 <71232870+hsedaqat1378@users.noreply.github.com>
2021-02-11 14:52:58 +0330

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,19 +1,38 @@
+// Main Vue App
 import Vue from 'vue'
 import App from './App.vue'
+
+// Vue-Router
 import Vuerouter from 'vue-router'
+
+// UI-BootstrapVue Added
 import BootstrapVue from 'bootstrap-vue'
+
+// Import Bootstarp for Use in All of Component (Global)
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Import Router Folder
 import { router } from './Router/router.js'
+
+// Import Vuex Folder
 import { store } from './Store/store.js'
+
+// Uses Bootsratp Vue
 Vue.use(BootstrapVue)
+
+// Use VueRouter
 Vue.use(Vuerouter)
+
 const routers = new Vuerouter({
   routes: router,
-  mode: 'history'
+  // Chnage Mode
+  mode: 'history',
 })
+
 Vue.config.productionTip = false
 
+// Render Main App-Vuex-VueRouter
 new Vue({
   render: h => h(App),
   router: routers,
