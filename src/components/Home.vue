9788781019bd229f7e0f6c9590b0d34debf914ce
hsedaqat1378 <71232870+hsedaqat1378@users.noreply.github.com>
2021-02-28 09:14:43 +0330

    Update Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,28 +6,4 @@
         <div class="col-md-10 mx-auto">
           <h3 class="mb-4">Welcome To Blog Post</h3>
           <p class="lead badge badge-success">Everythings Your Mind Post it...</p>
-          <div class="form-group">
-            <label for="title">Post Title:</label>
-            <input type="text" v-model="posts.title" class="form-control col-md-10" />
-          </div>
-          <div class="form-group">
-            <label for="text">Whats Your Mind?</label>
-            <textarea
-              name=""
-              id=""
-              cols="30"
-              rows="10"
-              class="form-control col-md-10"
-              v-model="posts.text"
-            ></textarea>
-          </div>
-          <button @click="add()" class="btn btn-info">Posted</button>
-        </div>
-      </div>
-    </div>
-  </div>
-</template>
-
-<script>
-import { mapActions,mapActions } from "vuex";
-import Header from "./Header.vue";
+          <
