8ce9519489ce6f20ca235b484403e8861a439dcb
hsedaqat1378 <71232870+hsedaqat1378@users.noreply.github.com>
2021-02-28 09:16:36 +0330

    Update Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,1 +1,37 @@
-
+<template>
+  <div>
+    <div id="mySpinner" v-if="mySpinner">
+      <div class="spinner-border" style="width: 3rem; height: 3rem" role="status">
+        <span class="sr-only">Loading...</span>
+      </div>
+    </div>
+    <div v-if="myContent">
+      <app-header></app-header>
+      <div class="container mt-4">
+        <div class="row">
+          <div class="col-md-10 mx-auto">
+            <h3 class="mb-4">Welcome To Blog Post</h3>
+            <p class="lead badge badge-success">Everythings Your Mind Post it...</p>
+            <div class="form-group">
+              <label for="title">Post Title:</label>
+              <input type="text" v-model="posts.title" class="form-control col-md-10" />
+            </div>
+            <div class="form-group">
+              <label for="text">Whats Your Mind?</label>
+              <textarea
+                name=""
+                id=""
+                cols="30"
+                rows="10"
+                class="form-control col-md-10"
+                v-model="posts.text"
+              ></textarea>
+            </div>
+            <button @click="add()" class="btn btn-info">Posted</button>
+          </div>
+        </div>
+      </div>
+      <app-footer></app-footer>
+    </div>
+  </div>
+</template>
