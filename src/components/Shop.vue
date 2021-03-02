75e314f8fe235506d82e06e06839193258282458
hsedaqat1378 <71232870+hsedaqat1378@users.noreply.github.com>
2021-03-02 10:07:55 +0330

    Update Shop.vue

--- src/components/Shop.vue
+++ src/components/Shop.vue
@@ -1,24 +1,2 @@
 <template>
   
-  
-    },
-    timeOutforcart() {
-      document.querySelector(".add").style.display = "block";
-      setTimeout(() => {
-        document.querySelector(".add").style.display = "none";
-      }, 3000);
-    },
-  },
-};
-</script>
-
-<style scoped>
-.add {
-  background: green;
-  color: #fff;
-  padding: 0.6rem;
-  border-radius: 6px;
-  width: 350px;
-  display: none;
-}
-</style>
