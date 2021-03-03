c942507c8191e534ede687bd9124e24e08f6655f
hsedaqat1378 <71232870+hsedaqat1378@users.noreply.github.com>
2021-03-03 10:03:47 +0330

    Update Setting.vue

--- src/components/Setting.vue
+++ src/components/Setting.vue
@@ -1,1 +1,33 @@
-
+<template>
+  <div>
+    <div id="mySpinner" v-if="mySpinner">
+      <div class="spinner-border" style="width: 3rem; height: 3rem" role="status">
+        <span class="sr-only">Loading...</span>
+      </div>
+    </div>
+    <div v-if="myContent">
+      <app-header></app-header>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 mx-auto">
+            <div class="card p-5 mt-4">
+              <h3>Setting Pages:</h3>
+              <p>Allow register Your Data For Better Help In Chosses for You:</p>
+              <div class="form-group">
+                <input type="radio" name="" id="" checked /> Yes
+              </div>
+              <div class="form-group"><input type="radio" name="" id="" /> No</div>
+              <!-- Another Setting -->
+              <p>Perfer Save your Data:</p>
+              <div class="form-group"><input type="radio" name="" id="" /> Yes</div>
+              <div class="form-group">
+                <input type="radio" name="" id="" checked /> No
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <app-footer></app-footer>
+    </div>
+  </div>
+</template>
