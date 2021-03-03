a3bf3183396784d9dea87d5a63460b47a669e2f8
hsedaqat1378 <71232870+hsedaqat1378@users.noreply.github.com>
2021-03-03 10:03:34 +0330

    Update Setting.vue

--- src/components/Setting.vue
+++ src/components/Setting.vue
@@ -1,9 +1,1 @@
-<template>
-  <div>
-    <app-header></app-header>
-    <div class="container">
-      <div class="row">
-        <div class="col-md-12 mx-auto">
-          <div class="card p-5 mt-4">
-            <h3>Setting Pages:</h3>
-        
+
