86c98d8f239b80e663966e26ccf40f21b6391058
hsedaqat1378 <71232870+hsedaqat1378@users.noreply.github.com>
2021-07-10 09:25:55 +0430

    Update Message.vue

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -53,7 +53,7 @@
                 type="text"
                 class="form-control"
                 placeholder="Message"
-                value="Hello erlich what's app My Man?"
+                value="Hello erlich what's My Man?"
               />
               <div class="input-group-append">
                 <button class="input-group-text bg-dark text-white" id="basic-addon2">
