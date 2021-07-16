0bcff3ddb6313484ca533267b18eb6b0179e7190
hsedaqat1378 <71232870+hsedaqat1378@users.noreply.github.com>
2021-07-16 11:55:26 +0430

    Update Setting.vue

--- src/components/Setting.vue
+++ src/components/Setting.vue
@@ -16,7 +16,7 @@
               <div class="form-group">
                 <input type="radio" checked /> Yes
               </div>
-              <div class="form-group"><input type="radio" name="/> No</div>
+              <div class="form-group"><input type="radio" name=/> No</div>
               <!-- Another Setting -->
               <p>Perfer Save your Data:</p>
               <div class="form-group"><input type="radio" name="" id="" /> Yes</div>
